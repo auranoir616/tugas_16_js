92aec3974030284a0a76647e3e4f1f1db08b2ccf
febri <fitrafilthmongers@gmail.com>
2023-07-19 12:59:22 +0700

    tugas 16 js
    mengumpulkan tugas 16 js

---
+++ tugas16.js
@@ -0,0 +1,5 @@
+var nilai1 = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+console.log('nilai minimal : ',nilai1)
+
+var nilai2 = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+console.log('nilai maksimal : ',nilai2)
